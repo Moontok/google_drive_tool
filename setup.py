7fbe9a8c03f1e15fff685e9bad975bfea5499543
Zackary <zspink@nea.k12.ar.us>
2022-12-16 13:48:25 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ from google_sheets_tool import __version__
 setup(
     name='google_sheets_tool',
     version=__version__,
-
+    install_requires=[
+        "googleapiclient"
+    ],
     url='https://github.com/Moontok/google_drive_tool',
     author='Zackary Spink',
     author_email='zspink@nea.k12.ar.us.com',

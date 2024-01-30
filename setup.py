1d0c47973922c10c4fe62e31318a13ced0742e65
Zackary <zspink@nea.k12.ar.us>
2024-01-29 20:50:40 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='google_sheets_tool',
+    name='google_drive_tool',
     version="1.0",
     packages=find_packages(),
     install_requires=[
